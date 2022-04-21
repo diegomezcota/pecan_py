5ddc0d62f57956e96df00d09c5fd9923119f3e8e
diegomezcota <gomezcotadiego@gmail.com>
2022-04-21 12:16:34 -0500

    Add functionality for testing individual files

--- ply/lexer_tests.py
+++ ply/lexer_tests.py
@@ -1,14 +1,15 @@
+import sys
 from lexer import lexer
 
 no_tests = 2
+test_no = str(sys.argv[1]) if len(sys.argv) > 1 else None
 test_files = ['test' + str(i) + '.gmc' for i in range(1, no_tests + 1)]
 
 print("---PROBANDO ANALIZADOR LÉXICO---")
 
-# Give the lexer some input
-for i, test in enumerate(test_files):
-    print("TEST no.", i + 1)
-    data = open('tests/lexer/' + test, 'r').read()
+if test_no:
+    print("TEST no.", test_no)
+    data = open('tests/lexer/' + 'test' + test_no + '.gmc', 'r').read()
     lexer.input(data)
     # Tokenize
     while True:
@@ -16,3 +17,15 @@ for i, test in enumerate(test_files):
         if not tok: 
             break      # No more input
         print(tok)
+else:
+    # Give the lexer some input
+    for i, test in enumerate(test_files):
+        print("TEST no.", i + 1)
+        data = open('tests/lexer/' + test, 'r').read()
+        lexer.input(data)
+        # Tokenize
+        while True:
+            tok = lexer.token()
+            if not tok: 
+                break      # No more input
+            print(tok)
