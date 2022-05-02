74577a3942f8c042e9939098d54b8fd0d370e5e9
diegomezcota <gomezcotadiego@gmail.com>
2022-05-02 16:38:40 -0500

    Add test 7 for changes

--- ply/parser_tests.py
+++ ply/parser_tests.py
@@ -3,7 +3,7 @@ import sys
 
 from pecan_parser import TypeMismatchError
 
-no_tests = 6
+no_tests = 7
 test_no = str(sys.argv[1]) if len(sys.argv) > 1 else None
 test_files = ['test' + str(i) + '.gmc' for i in range(1, no_tests + 1)]
 
