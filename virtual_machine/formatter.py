ffaaaa9040409d3b39b8d6fbba67186bf06adfc0
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-05-20 20:14:15 -0500

    Started linear statements execution

--- virtual_machine/formatter.py
+++ virtual_machine/formatter.py
@@ -1,16 +1,13 @@
 class Formatter:
     def __init__(self):
         ...
-    
+
     def cast(self, value, type):
         if type == 'int':
             return int(value)
         if type == 'float':
             return float(value)
         if type == 'bool':
-            if value == 'true':
-                return True
-            return False
+            return value
         if type == 'string':
             return value
-        
